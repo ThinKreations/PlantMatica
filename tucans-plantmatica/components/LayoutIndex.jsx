551421abc624bb0e6afb18641d076c2a2e67da02
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-11-15 13:55:10 -0600

    wiiiiiiiiiiii

---
+++ tucans-plantmatica/components/LayoutIndex.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import logo_1_w from "../src/logo_1_w.png";
+import git_logo from "../src/git_icon.png";
+
+export default function LayoutIndex({ children }) {
+    return (
+        <>
+            <div className={styles.container_0} style={{margin: '0', padding: '0'}} >
+                <font face="Work Sans" color="white">
+                    <header className={styles.head_0}>
+                        <br/><Link href="/"><Image src={logo_1_w} width={'200%'} height={'80%'}  /></Link>
+                    </header>
+                </font>
+                { children }
+                <font face="Work Sans" color="#FAFAFA">
+                    <footer className={styles.footy}>
+                        <table className={styles.footy_t}>
+                            <tbody><tr>
+                                <td className={styles.footy_t_1}>
+                                    <a><h2>© 2021 Tucan's Software</h2></a>
+                                </td>
+                                <td className={styles.footy_t_2}>
+                                    <Link href="https://github.com/addRian0-0/5IV9-PA-Tucanes-del-Software" passHref>
+                                        <a target="_blank">
+                                        <Image src={git_logo} className={styles.git_logo} width={'50%'} height={'50%'}/>
+                                        </a>
+                                    </Link>
+                                </td>
+                            </tr>
+                            </tbody></table>
+                    </footer>
+                </font>
+            </div>
+        </>
+    )
+}
---
+++ tucans-plantmatica/components/MainHead.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from "next/head";
+import icon from "../public/favicon.ico";
+export default function MainHead(props) {
+    return (
+        <div>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title> {props.tituloPestana} </title>
+                <link rel="shortcut icon" href={props.icon} />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;1,300&display=swap" rel="stylesheet"></link>
+            </Head>
+        </div>
+    )
+}
