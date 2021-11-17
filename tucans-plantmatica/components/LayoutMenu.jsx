addcf708f4eea41c97116760e18660c6e13b5489
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-11-17 03:38:17 -0600

    menuSup pt. 1

--- tucans-plantmatica/components/LayoutMenu.jsx
+++ tucans-plantmatica/components/LayoutMenu.jsx
@@ -26,38 +26,24 @@ export default function Menusup({ children }) {
             <div className={styles.container_0} style={{ margin: '0', padding: '0' }} >
                 <font face="Work Sans" color="white">
                     <div className={styles.head}>
-                        <div>
-                            <br/><Link href="#"><Image src={logo_1_w} width={'188%'} height={'70%'} /></Link>
+                        <div className={styles.logo1}>
+                           <Link href="#" ><Image src={logo_1_w} width={'148%'} height={'60%'}  /></Link>
                         </div>
+                        <font face="Work Sans" color="white">
                         <div className={styles.botonera}>
-                            <Link href="/fichas">
-                                <a>
-                                    <button className={styles.menuButton}>
-                                        <font face="Work Sans" color="white"><h3>Inicio</h3></font>
-                                    </button>
-                                </a>
-                            </Link>
-                            <Link href="/user/">
-                                <a>
-                                    <button className={styles.menuButton}>
-                                        <font face="Work Sans" color="white"><h3>Mi Cuenta</h3></font>
-                                    </button>
-                                </a>
-                            </Link>
-                            <Link href="/user/fichasGuardadas">
-                                <a>
-                                    <button className={styles.menuButton}>
-                                        <font face="Work Sans" color="white"><h3>Fichas guardadas</h3></font>
-                                    </button>
-                                </a>
-                            </Link>
-                            <Link href="/">
-                                <a>
-                                    <button className={styles.menuButton} onClick={handleToggle}>
-                                        <font face="Work Sans" color="white"><h3>{`Cerrar Sesión`}</h3></font>
-                                    </button>
-                                </a>
-                            </Link>
+                            <div className={styles.dropdown}>
+                            <button className={styles.dropbtn}><font face="Work Sans" color="white"><b>{`Inicio`}</b></font></button>
+                            
+                            </div>
+                            <div className={styles.dropdown}>
+                            <button className={styles.dropbtn}><font face="Work Sans" color="white"><b>{`Cuenta`}</b></font></button>
+                                <div className={styles.dropdownContent}>
+                                    <a href="#">{`Link 1`}</a>
+                                    <a href="#">{`Link 2`}</a>
+                                    <a href="#">{`Link 3`}</a>
+                                </div>
+                            </div>
+                            
                             <Backdrop
                                 sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                                 open={open}
@@ -66,31 +52,12 @@ export default function Menusup({ children }) {
                                 <CircularProgress color="inherit" />
                             </Backdrop>
                         </div>
-
+                        </font>
                     </div>
 
                 </font>
-                <div className={styles2.bgimagen}>
-                    {children}
-                </div>
-                <font face="Work Sans" color="#FAFAFA">
-                    <footer className={styles2.footy}>
-                        <table className={styles2.footy_t}>
-                            <tbody><tr>
-                                <td className={styles2.footy_t_1}>
-                                    <a><h2>{`© 2021 Tucan's Software`}</h2></a>
-                                </td>
-                                <td className={styles2.footy_t_2}>
-                                    <Link href="https://github.com/addRian0-0/5IV9-PA-Tucanes-del-Software" passHref>
-                                        <a target="_blank">
-                                            <Image src={git_logo} className={styles2.git_logo} width={'50%'} height={'50%'} />
-                                        </a>
-                                    </Link>
-                                </td>
-                            </tr>
-                            </tbody></table>
-                    </footer>
-                </font>
+                
+                
             </div>
         </>
 
--- tucans-plantmatica/pages/fichas/index.jsx
+++ tucans-plantmatica/pages/fichas/index.jsx
@@ -9,9 +9,9 @@ export default function Index() {
     return (
         <div>
             <MainHead tituloPestana="Inicio" />
-            <LayoutIndex>
+            <LayoutMenu/>
                 <MostrarFicha/>
-            </LayoutIndex>
+            
         </div>
     )
 }
