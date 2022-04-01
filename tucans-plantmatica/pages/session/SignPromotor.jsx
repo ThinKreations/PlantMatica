155aaa91771495ae3b32d34c5c6a4d802a6b2c2c
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-04-01 17:09:32 -0600

    nosevalidarayuda

--- tucans-plantmatica/pages/session/SignPromotor.jsx
+++ tucans-plantmatica/pages/session/SignPromotor.jsx
@@ -7,13 +7,13 @@ import Link from 'next/link';
 import Router from "next/router";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
-import { schemaCrearCuenta } from "../../schemas/crearCuenta";
+import { schemaSignPromotor } from "../../schemas/signPromotor";
 import swal from "sweetalert";
 
 export default function SignPromotor() {
 
     const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schemaCrearCuenta)
+        resolver: yupResolver(schemaSignPromotor)
     });
 
     const onSubmit = async (data) => {
@@ -67,53 +67,53 @@ export default function SignPromotor() {
                     <center><p>Por favor, llena este formulario con los datos solicitados. <b/>En caso de necesitar aclaraciones, envíe un correo a soporte. (<a className={styles.link} href="mailto:software.tucans@gmail.com">software.tucans@gmail.com</a>)</p></center>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Nombre  público</label>
-                        <input {...register('username')} className={styles.input} />
-                        <p className={styles.errors} >{errors.username?.message}</p>
+                        <input {...register('nombre_publico')} className={styles.input} />
+                        <p className={styles.errors} >{errors.nombre_publico?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Razón social</label>
-                        <input type="text" className={styles.input} />
-                        
+                        <input {...register('razon_social')} type="text" className={styles.input} />
+                        <p className={styles.errors} >{errors.razon_social?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Usuario de Referencia</label>
-                        <input {...register('correo')} className={styles.input} />
-                        <p className={styles.errors} >{errors.correo?.message}</p>
+                        <input {...register('usuario_referencia')} className={styles.input} />
+                        <p className={styles.errors} >{errors.usuario_referencia?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >{`Dirección comercial`}</label>
-                        <input {...register('password')} type="text" className={styles.input} />
-                        <p className={styles.errors} >{errors.password?.message}</p>
+                        <input {...register('direccion_comercial')} type="text" className={styles.input} />
+                        <p className={styles.errors} >{errors.direccion_comercial?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Teléfono comercial</label>
-                        <input {...register('passwordV')} type="number" className={styles.input} />
-                        <p className={styles.errors} >{errors.passwordV?.message}</p>
+                        <input {...register('telefono_comercial')} type="number" className={styles.input} />
+                        <p className={styles.errors} >{errors.telefono_comercial?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Correo comercial</label>
-                        <input {...register('passwordV')} type="text" className={styles.input} />
-                        <p className={styles.errors} >{errors.passwordV?.message}</p>
+                        <input {...register('correo')} type="text" className={styles.input} />
+                        <p className={styles.errors} >{errors.correo?.message}</p>
                     </div>
                     
                     
                     <div className={styles.cont_input}>
                         <label className={styles.text} >CLABE interbancaria</label>
-                        <input {...register('passwordV')} type="number" className={styles.input} />
-                        <p className={styles.errors} >{errors.passwordV?.message}</p>
+                        <input {...register('clabe_interbancaria')} type="number" className={styles.input} />
+                        <p className={styles.errors} >{errors.clabe_interbancaria?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >Tarjeta de crédito del titular</label>
-                        <input {...register('passwordV')} type="number" className={styles.input} />
-                        <p className={styles.errors} >{errors.passwordV?.message}</p>
+                        <input {...register('tarjeta_credito')} type="number" className={styles.input} />
+                        <p className={styles.errors} >{errors.tarjeta_credito?.message}</p>
                     </div>
                     <div className={styles.cont_input}>
                         <label className={styles.text} >RFC</label>
-                        <input type="text" className={styles.input} />
-                        
+                        <input {...register('rfc')} type="text" className={styles.input} />
+                        <p className={styles.errors} >{errors.rfc?.message}</p>
                     </div>
                     
-                    <Link href="../promo/"><button type='submit' className={styles.btnSubmit} >Crear Cuenta</button></Link>
+                    <button type='submit' className={styles.btnSubmit} >Crear Cuenta</button>
                     
                     
                 </form>
