a04bfc282fa1556a2d3dcaaae219e133b88d11ad
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-05-01 21:09:26 -0500

    iniciando captura de valores de sucursales

--- tucans-plantmatica/pages/user/promo/sucursales.jsx
+++ tucans-plantmatica/pages/user/promo/sucursales.jsx
@@ -27,24 +27,24 @@ export default function Sucursales () {
         <div className={styles.ficha}>
         <aside className={styles.dataSucursal}>
         <font size={2} face="Work Sans" color="007200">
-        <h1>`Nombre de la Sucursal`</h1>
-        <h2>`Empresa o Promotor`</h2>
-        <h3>`Dirección Comercial`</h3>
-        <h3>`Teléfono Comercial`</h3>
-        <h3>Atendido por `Encargado`</h3>
-        <h3>Servicio a domicilio: `Sí/No`</h3>
+        <h1>La Oficina</h1>
+        <h2>Tucan's Software</h2>
+        <h3>Ciudad de México, Miguel Hidalgo, Mar Mediterráneo #227</h3>
+        <h3>5557296000</h3>
+        <h3>Atendido por Adrián Flores</h3>
+        <h3>Servicio a domicilio: No</h3>
         </font>
         </aside>  
         <aside className={styles.dataSucursal}>
         <font size={2} face="Work Sans" color="007200">
         <h2>Horario</h2>
-        <h3>Lunes: `1` a `2`</h3>
-        <h3>Martes: `1` a `2`</h3>
-        <h3>Miércoles: `1` a `2`</h3>
-        <h3>Jueves: `1` a `2`</h3>
-        <h3>Viernes: `1` a `2`</h3>
-        <h3>Sábado: `1` a `2`</h3>
-        <h3>Domingo: `1` a `2`</h3>
+        <h3>Lunes: 13:00 a 21:00</h3>
+        <h3>Martes: 13:00 a 21:00</h3>
+        <h3>Miércoles: 13:00 a 21:00</h3>
+        <h3>Jueves: 14:00 a 21:00</h3>
+        <h3>Viernes: 14:00 a 21:00</h3>
+        <h3>Sábado: Sin servicio</h3>
+        <h3>Domingo: Sin servicio</h3>
         </font>
         </aside>  
         <div>
@@ -62,24 +62,24 @@ export default function Sucursales () {
         <div className={styles.ficha}>
         <aside className={styles.dataSucursal}>
         <font size={2} face="Work Sans" color="007200">
-        <h1>`Nombre de la Sucursal`</h1>
-        <h2>`Empresa o Promotor`</h2>
-        <h3>`Dirección Comercial`</h3>
-        <h3>`Teléfono Comercial`</h3>
-        <h3>Atendido por `Encargado`</h3>
-        <h3>Servicio a domicilio: `Sí/No`</h3>
+        <h1>El Centro Médico</h1>
+        <h2>Tucan's Software</h2>
+        <h3>Ciudad de México, Miguel Hidalgo, Mar Mediterráneo #227</h3>
+        <h3>1234567890</h3>
+        <h3>Atendido por Fetu Ccini</h3>
+        <h3>Servicio a domicilio: Sí</h3>
         </font>
         </aside>  
         <aside className={styles.dataSucursal}>
         <font size={2} face="Work Sans" color="007200">
         <h2>Horario</h2>
-        <h3>Lunes: `1` a `2`</h3>
-        <h3>Martes: `1` a `2`</h3>
-        <h3>Miércoles: `1` a `2`</h3>
-        <h3>Jueves: `1` a `2`</h3>
-        <h3>Viernes: `1` a `2`</h3>
-        <h3>Sábado: `1` a `2`</h3>
-        <h3>Domingo: `1` a `2`</h3>
+        <h3>Lunes: 13:00 a 21:00</h3>
+        <h3>Martes: 13:00 a 21:00</h3>
+        <h3>Miércoles: 13:00 a 21:00</h3>
+        <h3>Jueves: 14:00 a 21:00</h3>
+        <h3>Viernes: 14:00 a 21:00</h3>
+        <h3>Sábado: Sin servicio</h3>
+        <h3>Domingo: Sin servicio</h3>
         </font>
         </aside>  
         <div>
